5bf4258d5b2ad58339b77975ff37ed4a6fa3dbbe
Jxxzyshn <71040617+jxxzy@users.noreply.github.com>
2024-02-15 18:56:41 +0700

    Update struct.go
    .

--- struct.go
+++ struct.go
@@ -96,7 +96,7 @@ type GeoJsonPolygon struct {
 
 type Point struct {
 	Coordinates []float64 `json:"coordinates" bson:"coordinates"`
-	Max         int64     `json:"max,omitempty" bson:"max,omitempty"`
+	Max         float64     `json:"max,omitempty" bson:"max,omitempty"`
 	Min         int64     `json:"min,omitempty" bson:"min,omitempty"`
 	Radius      float64   `json:"radius,omitempty" bson:"radius,omitempty"`
 }
